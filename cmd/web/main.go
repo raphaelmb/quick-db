df2afa6b78e41bf8ce8b958adb71474edc65e700
Raphael Berger <raphaelmb@gmail.com>
2024-03-15 09:38:59 -0300

    Starting web

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,17 +1,53 @@
 package main
 
 import (
+	"embed"
+	"fmt"
+	"log"
 	"net/http"
-
-	"github.com/raphaelmb/quick-db/internal/database"
-	"github.com/raphaelmb/quick-db/internal/sdk"
+	"text/template"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		sdk.Setup(database.NewMySQL("mysql", "", "", "", "", "", "", false))
-		w.WriteHeader(http.StatusOK)
-	})
+//go:embed template/*
+var templateFS embed.FS
 
-	http.ListenAndServe(":9000", nil)
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /", home)
+	mux.HandleFunc("POST /create", create)
+	mux.HandleFunc("GET /list", list)
+	mux.HandleFunc("POST /remove", remove)
+
+	if err := http.ListenAndServe(":9000", mux); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func create(w http.ResponseWriter, r *http.Request) {
+	// TODO
+}
+
+func list(w http.ResponseWriter, r *http.Request) {
+	// TODO
+}
+
+func remove(w http.ResponseWriter, r *http.Request) {
+	// TODO
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	render(w, "template.html")
+}
+
+func render(w http.ResponseWriter, t string) {
+	tmpl, err := template.ParseFS(templateFS, fmt.Sprintf("template/%s", t))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if err := tmpl.Execute(w, ""); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
